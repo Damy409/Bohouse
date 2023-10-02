306afe17a32af4cd865c5b60c38d63fe73c5689e
unknown <damyvillegas7@gmail.com>
2023-10-02 18:46:56 -0500

    Primer intento

---
+++ src/ui/Main.java
@@ -0,0 +1,15 @@
+package ui;
+
+import java.util.Scanner;
+import model.Controller;
+
+public class Main{
+
+    private Scanner reader;
+    private Controller controller;
+
+    public Main(){
+
+        
+    }
+}
