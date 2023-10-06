b4f2ebee050e9890aed2a2b8580a7adae152bff8
unknown <damyvillegas7@gmail.com>
2023-10-06 02:52:36 -0500

    Correcciones

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 
 public class Controller<K, V> {
 
-    int taskCounter = -1;
-
     private HashTable<Integer, String> taskTable;
     int taskCounter = -1;
 
@@ -16,10 +14,7 @@ public class Controller<K, V> {
     public Controller(){
 
         taskTable = new HashTable<>(5);
-<<<<<<< HEAD
 
-=======
->>>>>>> f77b825a4cfa3e060992e2d281ab75761531d44a
 
     }
 
@@ -27,57 +22,23 @@ public class Controller<K, V> {
 
         Task task = new Task(title, description, date, priorityLevel);
 
-        taskCounter = taskCounter+5;
+        int priorityValue = (priorityLevel == PriorityLevel.HIGH) ? 0:1;
 
-<<<<<<< HEAD
         taskCounter++;
 
           //Preguntar que se pasa en el value y como pasar una task en value
 
         taskTable.put(taskCounter, title);
-=======
-        int taksValue = taskCounter;
->>>>>>> f77b825a4cfa3e060992e2d281ab75761531d44a
 
-        taskTable.insert(taksValue, title, null);        //Preguntar que se pasa en el value y como pasar una task en value
 
         return "The task / reminder " +title+ "has been created ";
     }  
      
 
-<<<<<<< HEAD
     public void printTasks() {
         
         taskTable.printTable();
         
-=======
-    public String printTasks(){
-
-        /*String tasks = taskTable.print();
-
-        System.out.println(tasks);
-        */
-
-        StringBuilder result = new StringBuilder("Tasks:\n");
-
-        // Itera sobre la HashTable
-        for (int i = 0; i < 5; i++) {
-            String task = taskTable.get(i);
-            if(task != null){
-                result.append(task).append("\n");
-            }
-
-            /*
-            while (currentNode != null) {
-                // Obtiene la tarea utilizando el método get y la agrega a la representación en forma de cadena
-                result.append(currentNode.toString()).append("\n");
-                currentNode = currentNode.getNext();
-            }
-            */
-        }
-    
-        return result.toString();
->>>>>>> f77b825a4cfa3e060992e2d281ab75761531d44a
     }
    
     
