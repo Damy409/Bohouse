ac364195534c7b0fdbea0ed1c25e0a9d8f3bb9bf
unknown <damyvillegas7@gmail.com>
2023-10-15 17:24:56 -0500

    Correcciones en controller

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -1,25 +1,61 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Controller<K, V> {
 
-    private ArrayList<HashNode<K,V>> organizationTable;
+    private HashTable<Integer, String> taskTable;
+    private PriorityQueue<Task> taskQueuePriority = new PriorityQueue<>();
+    int taskCounter = -1;
+    private Queue<Task> taskQueue;
 
-    HashTable hashTable = new HashTable();
+    //private ArrayList<HashNode<K,V>> organizationTable;
 
-    public String createTask(String title, String description, String date, PriorityLevel priorityLevel){
+    //HashTable hashTable = new HashTable();
 
-        Task task = new Task(title, description, date, priorityLevel);
+    public Controller(){
 
-        int key = hashTable.hash(task);
+        taskTable = new HashTable<>(5);
+        taskQueue = new Queue<>();
+        taskQueuePriority = new PriorityQueue<>();
+    }
 
-        //Preguntar que se pasa en el value y como pasar una task en value
+    public String createTask(String title, String description, String date, PriorityLevel priorityLevel, int priorityOrder) throws Exception{
 
+        Task task = new Task(title, description, date, priorityLevel, priorityOrder);
+
+        int priorityValue = (priorityLevel == PriorityLevel.HIGH) ? 0:1;
+
+        taskCounter++;
+
+          //Preguntar que se pasa en el value y como pasar una task en value
+
+        taskTable.put(taskCounter, title);
+
+        if (task.getPriority().equals(PriorityLevel.LOW)) {
+            taskQueue.enqueue(task);
+        }
+
+        if (task.getPriority().equals(PriorityLevel.HIGH)) {
+            taskQueuePriority.enqueue(task); // Agrega la tarea a la cola personalizada
+        }
 
 
         return "The task / reminder " +title+ "has been created ";
-    }   
-    
+    }  
+     
 
+    public void printTasks() {
+        
+        System.out.println("HASH TABLE");
+        taskTable.printTable();
+        System.out.println("");
+        System.out.println("QUEUE NON PRIORITY");
+        taskQueue.printQueue();
+        System.out.println("QUEUE PRIORITY (HIGH)");
+        taskQueuePriority.printQueue();
+        
+    }
+   
 }
